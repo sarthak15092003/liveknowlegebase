fb1f494e5bd7b6c32d343acfb94a77ed44524cf4
sarthak garg <Sarthakgarg1122@gmail.com>
2026-08-03 17:05:14 +0530

    Remove padding-top: 30px from blog_area section

--- single.php
+++ single.php
@@ -40,7 +40,7 @@ get_template_part( 'template-parts/single-post/banner', $banner_type );
 //     get_template_part( 'template-parts/header-elements/search-banner/breadcrumb' );
 // }
 ?>
-<section class="blog_area tip_doc_area" id="toc_stick" style="padding-top: 30px !important;">
+<section class="blog_area tip_doc_area" id="toc_stick">
 
     <div class="container">
         <div class="row align-items-start">
